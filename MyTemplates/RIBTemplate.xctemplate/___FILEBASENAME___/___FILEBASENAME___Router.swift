923dcd5264be86be3dae3bda2f7c2e755e46e061
Parsa <63924414+Parsa76@users.noreply.github.com>
2025-07-23 11:45:45 +0200

    add RIB Template and VIPER Template

---
+++ MyTemplates/RIBTemplate.xctemplate/___FILEBASENAME___/___FILEBASENAME___Router.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+@MainActor
+struct ___VARIABLE_productName:identifier___Router: GlobalRouter {
+    let router: Router
+    let builder: ___VARIABLE_productName:identifier___Bulider
+}
